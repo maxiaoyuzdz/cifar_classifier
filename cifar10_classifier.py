69ede33052dab038909e6535014e656b1ed6cfe9
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2017-07-14 16:14:27 +0800

    [save]

--- cifar10_classifier.py
+++ cifar10_classifier.py
@@ -1,4 +1,5 @@
 import sys
+import time
 import argparse
 import torch
 import torchvision
@@ -7,37 +8,65 @@ from torch.autograd import Variable
 import torch.nn as nn
 import torch.optim as optim
 
-from datautils import getTransform, getTransformV1
+import torch.backends.cudnn as cudnn
+
+from datautils import *
 from torch.utils.data.sampler import RandomSampler, SubsetRandomSampler
 from logoperator import SaveLog
 
 from cifarclassifier import Cifar10Classifier, Cifar10ClassifierV1
 
 import cifarclassifier
+from averagemeter import AverageMeter
 
 
 parser = argparse.ArgumentParser(description='Process training arguments')
 parser.add_argument('-e', '--epoch', default=300, type=int)
 parser.add_argument('-mb', '--mini_batch_size', default=128, type=int)
-parser.add_argument('-tb', '--test_batch_size', default=4, type=int)
-parser.add_argument('-lr', '--learning_rate', default=0.01, type=float)
+parser.add_argument('-tb', '--test_batch_size', default=128, type=int)
+parser.add_argument('-lr', '--learning_rate', default=0.05, type=float)
+parser.add_argument('-mu', '--momentum', default=0.9, type=float)
+
 parser.add_argument('-al', '--adjust_lr', default=0, type=int)
-parser.add_argument('-ap', '--adjust_period', default=10, type=int)
-parser.add_argument('-ar', '--adjust_rate', default=0.1, type=float)
+parser.add_argument('-ap', '--adjust_period', default=30, type=int)
+parser.add_argument('-ar', '--adjust_rate', default=0.5, type=float)
+
+parser.add_argument('-w', '--loader_worker', default=4, type=int)
 
 parser.add_argument('-logdir', '--log_dir', default='/media/maxiaoyu/data/Log/')
 parser.add_argument('-log', '--log_file_name', default='running.log')
 
 parser.add_argument('-d', '--data_path', default='/media/maxiaoyu/data/training_data')
+parser.add_argument('-pf', '--print_freq', default=128, type=int)
 
 
-def adjustlearningrate1(op, blr, rate, epoch, p):
-    lr = blr * (rate ** (epoch // p))
-    print('new lr = ', lr, ' basic lr = ', blr, ' epoch = ', epoch)
+
+def accuracy(output, target, topk=(1,)):
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum()
+        res.append( correct_k.mul_(100.0 / batch_size) )
+
+    return res
+
+
+
+
+
+def adjustLearningRatePeriodically(op, epoch):
+    lr = args.learning_rate * (args.adjust_rate ** (epoch // args.adjust_period))
+    print('new lr = ', lr, ' basic lr = ', args.learning_rate, ' epoch = ', epoch)
     for param_group in op.param_groups:
         param_group['lr'] = lr
 
-def adjustlearningrate2(op, epoch):
+def adjustLearningRateManually(op, epoch):
     if epoch >=150:
         lr = 0.01
         if epoch >= 150 and epoch < 250:
@@ -49,90 +78,63 @@ def adjustlearningrate2(op, epoch):
 
 
 
-def adjustlearningratecontrol(op, epoch):
+def adjustLearningRateControl(op, epoch):
 
     if args.adjust_lr == 0:
         pass
     elif args.adjust_lr == 1:
-        adjustlearningrate1(op, args.learning_rate, args.adjust_rate, epoch, args.adjust_period)
+        adjustLearningRatePeriodically(op, epoch)
 
     elif args.adjust_lr == 2:
-        adjustlearningrate2(op, epoch)
+        adjustLearningRateManually(op, epoch)
 
-def testdata():
-    transform = getTransform()
-    trainset = torchvision.datasets.CIFAR10(root=args.data_path,
-                                            train=True, download=False, transform=transform)
 
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=1,
-                                              shuffle=True, num_workers=2)
-
-    for index, data in enumerate(trainloader):
-        inputs, targets = data
-        print(inputs)
-        print(targets)
-        break
+def runTraining():
 
 
 
+    #training set
+    training_transforms = getTransformsForTraining()
+    training_set = torchvision.datasets.CIFAR10(root=args.data_path,
+                                            train=True, download=False, transform=training_transforms)
+    training_set_loader = torch.utils.data.DataLoader(training_set, batch_size=args.mini_batch_size,
+                                              shuffle=True, num_workers=args.loader_worker)
+    # validation set
+    validation_transforms = getTransformsForValidation()
+    val_set = torchvision.datasets.CIFAR10(root=args.data_path, train=False, download=False,
+                                          transform=validation_transforms)
+    val_set_loader = torch.utils.data.DataLoader(val_set, batch_size=args.mini_batch_size,
+                                              shuffle=False, num_workers=args.loader_worker)
 
-def runtraining(net, epoch_num, mini_batch_size, test_batch_size, learning_rate, log_path):
-
-    transform = getTransformV1()
-    trainset = torchvision.datasets.CIFAR10(root=args.data_path,
-                                            train=True, download=False, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=mini_batch_size,
-                                              shuffle=True, num_workers=2)
+    #test set
     testset = torchvision.datasets.CIFAR10(root=args.data_path, train=False,
-                                           download=False, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=test_batch_size,
-                                             shuffle=False, num_workers=2)
+                                           download=False, transform=validation_transforms)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=args.test_batch_size,
+                                             shuffle=False, num_workers=args.loader_worker)
 
-    #net = Cifar10ClassifierV1()
-    #net.cuda()
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+    # prepare model
+    model = cifarclassifier.__dict__['vgg11']()
+    # model.features = torch.nn.DataParallel(model.features)
+    model.cuda()
+    cudnn.benchmark = True
 
-    for epoch in range(epoch_num):
+    criterion = nn.CrossEntropyLoss().cuda()
+    optimizer = optim.SGD(model.parameters(), lr=args.learning_rate, momentum=args.momentum)
+
+    for epoch in range(args.epoch):
         running_loss = 0.0
         val_loss = 0.0
         computed_training_loss = 0
         computed_val_loss = 0
+
         #adjust learning rate
-        adjustlearningratecontrol(optimizer, epoch)
+        adjustLearningRateControl(optimizer, epoch)
 
         #training
-        for index, data in enumerate(trainloader):
-            if index < 4000:
-                inputs, targets = data
-                inputs, targets = Variable(inputs.cuda()), Variable(targets.cuda())
+        train(training_set_loader, net, )
 
-                optimizer.zero_grad()
-                output = net(inputs)
-                loss = criterion(output, targets)
-                loss.backward()
-                optimizer.step()
 
-                running_loss += loss.data[0]
-
-                if index % 2000 == 1999:
-                    print('[%d, %5d] training loss: %.16f' %
-                          (epoch + 1, index + 1, running_loss / 2000))
-                    computed_training_loss = running_loss / 2000
-                    running_loss = 0.0
-
-            if index >= 4000:
-                inputs, targets = data
-                inputs, targets = Variable(inputs.cuda()), Variable(targets.cuda())
-                output = net(inputs)
-                loss = criterion(output, targets)
-                val_loss += loss.data[0]
-                if index % 1000 == 999:
-                    print('[%d, %5d] validation loss: %.16f' %
-                          (epoch + 1, index + 1, val_loss / 1000))
-                    computed_val_loss = val_loss / 1000
-                    val_loss = 0.0
 
         # print epoch loss
         print('training loss = %.16f, validation loss = %0.16f' % (computed_training_loss, computed_val_loss))
@@ -148,7 +150,7 @@ def runtraining(net, epoch_num, mini_batch_size, test_batch_size, learning_rate,
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
 
-            for j in range(test_batch_size):
+            for j in range(args.test_batch_size):
                 if labels[j] == predicted[j][0]:
                     correct += 1
         test_accurate = 100 * correct / total
@@ -156,8 +158,59 @@ def runtraining(net, epoch_num, mini_batch_size, test_batch_size, learning_rate,
             100 * correct / total))
 
         #save log
-        SaveLog(epoch + 1, 5000, computed_training_loss, computed_val_loss, test_accurate, log_path)
+        SaveLog(epoch + 1, 5000, computed_training_loss, computed_val_loss, test_accurate,
+                args.log_dir + args.log_file_name )
         print('save log end')
+
+
+def train(training_set_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+
+
+    model.train()
+    end = time.time()
+
+    for i, (input, target) in enumerate(training_set_loader):
+        data_time.update(time.time() - end)
+
+        input_var, target_var = Variable(input.cuda()), Variable(target.cuda())
+        optimizer.zero_grad()
+        output = model(input_var)
+        loss = criterion(output, target_var)
+        loss.backward()
+        optimizer.step()
+
+
+        #transfer Variable to float Varibale
+        output_value = output.float()
+        loss_value = loss.float()
+
+        #measure accuracy
+        prec1 = accuracy(output_value.data, target)[0]
+        losses.update(loss_value[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+
+        #measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                  epoch, i, len(training_set_loader), batch_time=batch_time,
+                  data_time=data_time, loss=losses, top1=top1))
+
+
+
+
+
+
 
 
 
@@ -167,12 +220,7 @@ def main():
     args = parser.parse_args()
     #print(args.log_dir + args.log_file_name)
     #testdata()
-    model = cifarclassifier.__dict__['vgg11']()
-    #print(model)
-    #model.features = torch.nn.DataParallel(model.features)
-    model.cuda()
-
-    runtraining(model, args.epoch, args.mini_batch_size, args.test_batch_size, args.learning_rate, args.log_dir + args.log_file_name)
+    runTraining()
 
 
 
--- datautils.py
+++ datautils.py
@@ -6,13 +6,22 @@ def getTransform():
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
-def getTransformV1():
+def getTransformsForTraining():
     return transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomCrop(32, 4),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
     ])
 
+
+def getTransformsForValidation():
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                             std=[0.229, 0.224, 0.225])
+    ])
 
 def count(iter):
     return sum(1 for _ in iter)
