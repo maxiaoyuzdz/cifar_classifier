dca89a36a9b84496b1e9bf708b458c99a0b3c7c7
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2017-07-19 23:07:47 +0800

    [update] add color jitter, but have not finish, the effect looks wired

--- colorjitteringexample.py
+++ colorjitteringexample.py
@@ -2,6 +2,7 @@
 import numpy as np
 import torch
 import torchvision
+import time
 
 
 
@@ -14,20 +15,40 @@ import matplotlib.pyplot as plt
 
 
 
-def imshow(inp, title=None):
-    inp = inp.numpy().transpose((1, 2, 0))
+def imshow(img, title=None):
+
     mean = np.array([0.485, 0.456, 0.406])
     std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    print(inp.shape)
+    img = std * img + mean
+    #print(img.shape)
 
-    plt.imshow(inp)
+    plt.imshow(img)
 
     if title is not None:
         plt.title(title)
 
     plt.pause(0.001)
 
+
+
+def imgJitter(img):
+    """ img is Tensor, need to transfer numpy ndarray"""
+    img = img.numpy()
+    img = img.transpose((1, 2, 0))
+    h, w, c = img.shape
+    print(h)
+    print(img.shape)
+    noise = np.random.randint(0, 50, (h, w))  # design jitter/noise here
+    zitter = np.zeros_like(img)
+    zitter[:, :, 1] = noise
+    print(zitter.shape)
+    noise_added = img + zitter
+    print(noise_added.shape)
+
+    combined = np.vstack((img[:17, :, :], noise_added[17:, :, :]))
+
+
+    return combined
 
 
 
@@ -39,6 +60,12 @@ def getTransformsForTest():
                              std=[0.229, 0.224, 0.225])
     ])
 
+def getTransformsForTest2():
+    return transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5])
+    ])
 
 def main():
     print('ok')
@@ -57,6 +84,8 @@ def main():
 
     for i, (input, target) in enumerate(data_set_loader):
         out = torchvision.utils.make_grid(input)
+
+        out = imgJitter(out)
         imshow(out, title=[x for x in target])
         break
 
