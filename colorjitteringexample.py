ce39fda4a9b3ca3d916987c72b0116c67e0c438b
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2017-07-21 00:01:58 +0800

    [update] implement 2 types transform

--- colorjitteringexample.py
+++ colorjitteringexample.py
@@ -67,17 +67,47 @@ def imgJitter(img):
 
 def myTransform(x):
 
-
-    x = Image.new("RGB", (x.width, x.height), "white")
+    #
+    if np.random.randint(0, 2) == 1:
+        x = x.rotate(45)
+    #x = Image.new("RGB", (x.width, x.height), "white")
 
     return x
 
 
+def verticalFlipTransform(img):
+    if np.random.randint(0, 2) == 1:
+        img = img.transpose(Image.FLIP_TOP_BOTTOM)
+    return img
+
+def rotate90Transform(img):
+    if np.random.randint(0, 2) == 1:
+        img = img.rotate(90)
+    return img
+
+
+def rotate180Transform(img):
+    if np.random.randint(0, 2) == 1:
+        img = img.rotate(180)
+    return img
+
+
+def noiseTransform(img):
+    if np.random.randint(0, 2) == 1:
+        noise = np.random.randint(0, 50, (img.height, img.width))  # design jitter/noise here
+        zitter = np.zeros_like(img)
+        zitter[:, :, 1] = noise
+        noise_added = img + zitter
+        return noise_added
+    return img
+
 
 def getTransformsForTest():
     return transforms.Compose([
-        transforms.Lambda(lambda x: myTransform(x)),
-        #transforms.RandomCrop(32, 4),
+        transforms.Lambda(lambda x: verticalFlipTransform(x)),
+        transforms.Lambda(lambda x: rotate90Transform(x)),
+        transforms.Lambda(lambda x: rotate180Transform(x)),
+        transforms.Lambda(lambda x: noiseTransform(x)),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
@@ -90,17 +120,17 @@ def imgtest1():
     transforms = getTransformsForTest()
     data_set = torchvision.datasets.CIFAR10(root='/media/maxiaoyu/data/training_data',
                                             train=False, download=False, transform=transforms)
-    data_set_loader = torch.utils.data.DataLoader(data_set, batch_size=128,
+    data_set_loader = torch.utils.data.DataLoader(data_set, batch_size=4,
                                                   shuffle=False, num_workers=4)
 
     # inputs, targets = next(iter(data_set_loader))
 
-    for i, (input, target) in enumerate(data_set_loader):
-        out = torchvision.utils.make_grid(input)
-
+    for i, (indata, target) in enumerate(data_set_loader):
+        out = torchvision.utils.make_grid(indata)
         #out = imgJitter(out)
         #print('img shape = ', out.shape)
-        imshow(out, title=[x for x in target])
+        imshow(out)
+        #input('enter to continue')
         break
 
 def main():
