b9bda70937f2251f4e6b373b4432c1f2bab16a8d
Xiaoyu Ma <maxiaoyuzdz@gmail.com>
2017-08-05 00:42:37 +0800

    [update]

--- cifar100cnn.py
+++ cifar100cnn.py
@@ -119,6 +119,240 @@ class CNNModelWide(nn.Module):
         x = self.classifier(x)
         return x
 
+
+
+class CNNModelWide1(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide1, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 2048),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(2048, 1024),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(1024, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
+
+class CNNModelWide2(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide2, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 1024),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(1024, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
+
+class CNNModelWide3(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide3, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 2048),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(2048, 512),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(512, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
+
+class CNNModelWide4(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide4, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 512),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(512, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
+
+
+class CNNModelWide5(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide5, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 8192),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(8192, 512),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(512, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
+
+class CNNModelWide6(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide6, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 16384),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(16384, 512),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(512, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
+
+class CNNModelWide7(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide7, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 8192),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(8192, 1024),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(1024, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
+
+class CNNModelWide8(nn.Module):
+    def __init__(self, features):
+        super(CNNModelWide8, self).__init__()
+        self.features = features
+        self.classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(512, 16384),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(16384, 1024),
+            nn.ReLU(True),
+            # ====================
+            nn.Linear(1024, 100),
+        )
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+                m.bias.data.zero_()
+
+    def forward(self, x):
+        x = self.features(x)
+        # print(' parameter1 = ', x.size())
+        x = x.view(x.size(0), -1)
+        # print(' parameter2 = ', x.size())
+        x = self.classifier(x)
+        return x
 # for cifar100
 """
 def vgg11():
@@ -143,4 +377,32 @@ def vgg16_bn_deep():
 
 def vgg16_bn_wide():
     return CNNModelWide(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide1():
+    return CNNModelWide1(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide2():
+    return CNNModelWide2(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide3():
+    return CNNModelWide3(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide4():
+    return CNNModelWide4(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide5():
+    return CNNModelWide5(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide6():
+    return CNNModelWide6(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide7():
+    return CNNModelWide7(make_layers(cfg['D'], batch_norm=True))
+
+def vgg16_bn_wide8():
+    return CNNModelWide8(make_layers(cfg['D'], batch_norm=True))
+
+
+def vgg19_bn_wide5():
+    return CNNModelWide5(make_layers(cfg['E'], batch_norm=True))
 
